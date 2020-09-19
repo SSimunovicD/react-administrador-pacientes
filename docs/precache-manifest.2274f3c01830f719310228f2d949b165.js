bef251e797663df11fac980a9d8ed2d499e6e2b9
Sebastián Simunovic <ssimunovic02@gmail.com>
2020-09-19 10:46:17 -0300

    Docs agregado

---
+++ docs/precache-manifest.2274f3c01830f719310228f2d949b165.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5db97174a172df31a8ae046aeee24419",
+    "url": "/index.html"
+  },
+  {
+    "revision": "316f18e1b727a11ec96f",
+    "url": "/static/css/main.4c798faf.chunk.css"
+  },
+  {
+    "revision": "2993ce32bd51d2211559",
+    "url": "/static/js/2.59e9d737.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.59e9d737.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "316f18e1b727a11ec96f",
+    "url": "/static/js/main.b344ca89.chunk.js"
+  },
+  {
+    "revision": "37e277c3b441572b9284",
+    "url": "/static/js/runtime-main.80385eae.js"
+  }
+]);
